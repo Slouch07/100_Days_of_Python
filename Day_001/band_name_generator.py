ebb12fcfc92090ebdf08f75f709f2265fd75a8e4
Ryan Bowler <ryanbowler@hotmail.com>
2020-12-06 17:11:19 -0800

    added code from day 1 project.

---
+++ Day_001/band_name_generator.py
@@ -0,0 +1,15 @@
+# A program that outputs a band name after the user provides the city they grew up in
+# and the name of their first pet.
+
+# Greets the user to the program.
+print("Welcome to the Band Name Generator!!")
+
+# Asks the user for the city that they grew up in.
+city = input("What city did you grow up in?\n")
+
+# Asks the user for the name of a pet.
+pet = input("What was the name of your first pet?\n")
+
+# Combines their input and prints their band name.
+band_name = city + " " + pet
+print("Your new band name could be: " + band_name)
