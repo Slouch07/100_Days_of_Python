9370b7b15b1d141ade1ebf787c64d6a06dcd139f
Ryan Bowler <ryanbowler@hotmail.com>
2020-12-07 22:06:50 -0800

    added project code

--- Day_002/tip_calculator.py
+++ Day_002/tip_calculator.py
@@ -12,9 +12,11 @@ tip_percent = input("What percentage tip would you like to give? 10, 12, or 15? 
 # Ask the user how many people will be splitting the bill.
 customers = input("How many people to split the bill? ")
 
-# Calculate the tip and ouput the amount for each customer.
+# Calculate the tip 
 tip_amount = (float(bill_amount) / int(customers)) * (int(tip_percent) / 100 + 1)
 
 # Round the tip amount to two decimal points.
 result = format(tip_amount, '.2f')
-print(f"Each customer should pay: {result}")
+
+# Output the amount for each customer.
+print(f"Each customer should pay: ${result}")
