fcce9386d70003b6ec36144014620bcc4ffa82a6
Ryan Bowler <ryanbowler@hotmail.com>
2020-12-13 22:52:22 -0800

    Added initial code for encrypt function.

---
+++ Day_008/caesar_cipher.py
@@ -0,0 +1,18 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+            'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+# Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+def encrypt(message, shift_amount):
+    cipher_text = ""
+    # Shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
+    for letter in message:
+        index = alphabet.index(letter)
+        cipher_letter = alphabet[index + shift_amount]
+        cipher_text += cipher_letter
+    print(f"The encoded text is: {cipher_text}")
+
+encrypt(message=text, shift_amount=shift)
