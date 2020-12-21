fb4059610c9d1fb7e5aae2905095f27952b3f5b5
Ryan Bowler <ryanbowler@hotmail.com>
2020-12-21 10:12:18 -0800

    Added code for program to function as it should.

--- Day_014/higher_lower_game.py
+++ Day_014/higher_lower_game.py
@@ -2,39 +2,51 @@
 import art, game_data, random, replit
 
 def select_entity(list):
+  """Selects a random entry from the data list."""
   entity = random.choice(list)
   return entity
 
 def get_entity_info(entity_info):
+  """Selects the name, description, and country from the entry."""
   name = entity_info["name"]
   description = entity_info["description"]
   country = entity_info["country"]
   return(f"{name}, a {description}, from {country}.")
 
-
 # Create a list for the selected entities.
 random_entities = []
+
+# Variable to contain players score.
 score = 0
 
+# Select entry for entity A and append to random_entities list.
 entity_a = select_entity(game_data.data)
 random_entities.append(entity_a)
 
-# If the two random entries are the same entry choose again.
+# Select entry for entity B random_entities list.
 entity_b = select_entity(game_data.data)
+random_entities.append(entity_b)
 
 game_over = False
 while not game_over:
+  
   # Print logo art
   print(art.logo)
 
-  while entity_a == entity_b:
+  # If right - print 'you're right!' and score total.
+  if score > 0:
+    print(f"You're right! Score: {score}.")
+
+  # If entry A and entry B are the same, re-select entry B.
+  if random_entities[0] == random_entities[1]:
+    random_entities.remove(entity_b)
     entity_b = select_entity(game_data.data)
-  random_entities.append(entity_b)
+    random_entities.append(entity_b)
   
   # Print 'compare A' dictionary contents.
   print(f"Compare A: {get_entity_info(random_entities[0])}")
 
-  # Print 'vs' logo art
+  # Print 'vs' art
   print(art.vs)
 
   # Print 'agaist B' dictionary contents.
@@ -48,7 +60,6 @@ while not game_over:
   follower_count_a = random_entities[0]["follower_count"]
   follower_count_b = random_entities[1]["follower_count"]
 
-  # If right - print 'you're right!' and score total.
   # Swap contents of 'B' to postion 'A'and repeat steps above.
   if player_guess == 'a' and follower_count_a > follower_count_b:
     score += 1
@@ -56,14 +67,12 @@ while not game_over:
     entity_b = select_entity(game_data.data)
     random_entities.append(entity_b)
     replit.clear()
-    print(f"You're right! Score: {score}.")
   elif player_guess == 'b' and follower_count_b > follower_count_a:
     score += 1
     random_entities.pop(0)
     entity_b = select_entity(game_data.data)
     random_entities.append(entity_b)
     replit.clear()
-    print(f"You're right! Score: {score}.")
   else:
     # If wrong - clear game contents and print 'Sorry, that's wrong. Final {score}.
     # End the game.
