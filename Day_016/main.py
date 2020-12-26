a8ff39dbbb97dae07e44564e0a6b85aed8763398
Ryan Bowler <ryanbowler@hotmail.com>
2020-12-25 20:53:55 -0800

    Updated main.py

--- Day_016/main.py
+++ Day_016/main.py
@@ -2,6 +2,7 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+
 menu = Menu()
 coffee_maker = CoffeeMaker()
 money_machine = MoneyMachine()
